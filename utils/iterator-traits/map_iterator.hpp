09c608534f42369e1fb1ea69cadd84a7c17774c3
Gharbaoui <mohamed9gharbaoui@gmail.com>
2021-11-22 07:09:48 +0100

    retry iterator

--- utils/iterator-traits/map_iterator.hpp
+++ utils/iterator-traits/map_iterator.hpp
@@ -21,13 +21,11 @@ class rb_iterator{
         rb_iterator() : current(NULL) {
 			last_node = _alloc.allocate(1);
 		}
-        rb_iterator(node *n, node *last_n) :
-		current(n) , last_node(last_n), right_most(NULL) {}
         rb_iterator(node *n, node *last_n, node *right_m) :
-		current(n) , last_node(last_n), right_most(right_m) {}
+		current(n) , last_node(last_n), most_node(right_m) {}
 
         rb_iterator(const rb_iterator &cp) :
-		current(cp.current) , last_node(cp.last_node), right_most (cp.right_most) {}
+		current(cp.current) , last_node(cp.last_node), most_node (cp.most_node) {}
 		
 		rb_iterator&	operator=(const rb_iterator &rhs)
 		{
@@ -35,14 +33,14 @@ class rb_iterator{
 			{
 				current = rhs.current;
 				last_node = rhs.last_node;
-				right_most = rhs.right_most;
+				most_node = rhs.most_node;
 			}
 			return *this;
 		}
 
 		operator const_iterator()
 		{
-			return const_iterator(current, last_node, right_most);
+			return const_iterator(current, last_node, most_node);
 		}
 
         reference operator*() const {return current->get_item();}
@@ -51,29 +49,11 @@ class rb_iterator{
 
 		rb_iterator&	operator++()
 		{
-			if (current == last_node)
-			{
-				current = NULL;
-			}
-			else if (current)
-			{
-				current = current->successor();
-				if (!current)
-					current = last_node;
-			}
 			return *this;
 		}
 
 		rb_iterator&	operator--()
 		{
-			if (current == last_node)
-			{
-				current = right_most;
-			}
-			else if (current)
-			{
-				current = current->predecessor();
-			}
 			return *this;
 		}
 
@@ -97,9 +77,38 @@ class rb_iterator{
 	private:
         node		*current;
 		node		*last_node;
-		node		*right_most;
+		node		*most_node;
 		typename Alloc::template rebind<node>::other		_alloc;
         
+};
+
+
+template <typename ITER>
+class	reverse_iterator{
+	public:
+		typedef	ITER													iterator_type;
+		typedef	typename ft::iterator_traits<ITER>::value_type			value_type;
+		typedef	typename ft::iterator_traits<ITER>::difference_type		difference_type;
+		typedef	typename ft::iterator_traits<ITER>::iterator_category	iterator_category;
+		typedef	typename ft::iterator_traits<ITER>::reference			reference;
+		typedef	typename ft::iterator_traits<ITER>::pointer				pointer;
+
+		reverse_iterator() : current() {}
+		reverse_iterator(const iterator_type &itr) : current(itr) {}
+
+		reference	operator*() const {return *current;}
+		reverse_iterator&	operator++()
+		{
+			--current;
+			return *this;
+		}
+		reverse_iterator&	operator--()
+		{
+			++current;
+			return *this;
+		}
+	private:
+		iterator_type	current;
 };
 }
 #endif
